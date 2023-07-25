bb5c4c6981fc1d08bbb3cd7204747880183c3dd3
Fleetwood <fleetwood.young.iv@gmail.com>
2023-07-25 12:37:17 -0500

    material table templating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+export interface OlympicCountry {
+  country_code: string;
+  country: string;
+  medals: number;
+}
+
+const ELEMENT_DATA: OlympicCountry[] = [
+    {
+      country: "Algeria",
+      country_code: "DZA",
+      medals: 248
+    },
+    {
+      country: "Argentina",
+      country_code: "ARG",
+      medals: 4278
+    },
+    {
+      country: "Armenia",
+      country_code: "ARM",
+      medals: 310
+    },
+    {
+      country: "Australia",
+      country_code: "AUS",
+      medals: 20274
+    },
+    {
+      country: "Austria",
+      country_code: "AUT",
+      medals: 2232
+    },
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tomato-frontend-angular';
+  displayedColumns: string[] = ['country_code', 'country', 'medals'];
+  dataSource = ELEMENT_DATA;
 }
